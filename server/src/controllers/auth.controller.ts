266988db0c598016b06c453be880e0a19878b335
suzeklf <suzeklf@gmail.com>
2022-04-09 21:27:53 -0400

    Got connection error

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -6,59 +6,106 @@ import { User } from '../interfaces';
 import { AuthService } from '../services/auth.service';
 import { MongodbService } from '../services/mongodb.service';
 import { TYPES } from '../types';
-
+import { Collection, MongoClient, WithId } from 'mongodb';
 
 @injectable()
-export class AuthController{
-    public constructor(@inject(TYPES.AuthService) private _authService: AuthService, 
-                       @inject(TYPES.MongodbService) private _mongodbService: MongodbService){
+export class AuthController {
+    public constructor(
+        @inject(TYPES.AuthService) private _authService: AuthService,
+        @inject(TYPES.MongodbService) private _mongodbService: MongodbService) {
         //empty
     }
 
-    public get router() : Router {
-        
+    public get router(): Router {
+
         const router: Router = Router();
 
         // -> /api/v1/auth/login
-        router.post('/login',async (req:Request, res: Response) => {
+        router.post('/login', async (req: Request, res: Response) => {
             const auth: Authentification = req.body;
 
             //TODO Trouver l'utilisateur dans la BD, si l'utilisateur est null retournez le code 403 
-            // 在数据库中查找用户，如果用户为空，返回代码403
-
-            //TODO Comparer le mot de passe de la BD avec le mot de passe de la requête, utiliser le auth.service
-            //Retournez le code 403 au besoin 比较DB密码和请求密码，使用auth.service
-            //如果需要，返回代码403
-
-            //TODO Générer le jeton de l'utilisateur à l'aide du service auth.service
-            // 使用auth.service生成用户的令牌
-
-            //TODO Retourner les informations de connexion de l'utilisateur (voir interface UserConnection) sous format json 
-            // 以json格式返回用户的连接信息（见UserConnection接口）。
-
-            res.json();
+            const user: WithId<User> | null = await this._mongodbService.getUserByUsername(auth.username)
+            if (!user) {
+                res.sendStatus(403)
+            } else {
+                //TODO Comparer le mot de passe de la BD avec le mot de passe de la requête, utiliser le auth.service
+                //Retournez le code 403 au besoin
+                try {
+                    const result: boolean = await this._authService.isPasswordValid(auth.password, user.hash);
+                    if (result) {
+                        //TODO Générer le jeton de l'utilisateur à l'aide du service auth.service
+                        // 使用auth.service生成用户的令牌
+                        const token: string = this._authService.generateToken(user._id.toString());
+                        const userConnection: UserConnection = {
+                            id: user._id,
+                            token: token,
+                            username: user.username
+                        };
+                        //TODO Retourner les informations de connexion de l'utilisateur (voir interface UserConnection) sous format json 
+                        // 以json格式返回用户的连接信息（见UserConnection接口）。
+                        console.log(userConnection);
+                        res.json(userConnection);
+                    } else {
+                        alert("Your username or password don't match! ")
+                        res.sendStatus(403);
+                    }
+                }
+                catch (err) {
+                    console.error("Encrypt Password error! " + err.message);
+                    res.sendStatus(403);
+                }
+            }
         });
-        
+
         // -> /api/v1/auth/signup
-        router.post('/signup',async (req:Request, res: Response) => {
+        router.post('/signup', async (req: Request, res: Response) => {
             const auth: Authentification = req.body;
-
             //TODO Valider que l'utilisateur (username) n'est pas déjà dans la BD
-            //Retounez un code 405 si déjà présent验证用户（用户名）是否已经在数据库中。
-            //如果已经存在，则返回一个405代码
+            //Retounez un code 405 si déjà présent
+            const user: WithId<User> | null = await this._mongodbService.getUserByUsername(auth.username)
+            if (user) {
+                alert("Usename already exists!")
+                res.sendStatus(405)
+            } else {
+                //TODO Chiffrer le mot de passe avec auth.service 用auth.service加密密码
+                const hash: string = await this._authService.encryptPassword(auth.password);
+                //TODO Ajouter l'utilisateur à la BD 将用户添加到数据库中
+                //Retounez un code 500 en cas de problème 在出现问题的情况下返回代码500
+                try {
+                    const newUser: User = { 
+                        username: auth.username,
+                        hash: hash
+                    }
+                    const addUser: WithId<User> | null = await this._mongodbService.createUser(newUser)
+                    //TODO Générer le jeton de l'utilisateur à l'aide du service auth.service 
+                    // 使用auth.service生成用户的令牌
+                                            console.log('%cauth.controller.ts line:83 addUser', 'color: #007acc;', addUser);
 
-            //TODO Chiffrer le mot de passe avec auth.service 用auth.service加密密码
+                    if(addUser!==null){
+                        console.log('%cauth.controller.ts line:83 addUser', 'color: #007acc;', addUser);
+                        const token: string = this._authService.generateToken(addUser._id.toString());
+                        const userConnection: UserConnection = {
+                            id: addUser._id,
+                            token: token,
+                            username: addUser.username
+                        };
+                        console.log('%cauth.controller.ts line:89 userConnection', 'color: #007acc;', userConnection);
+                        res.json(userConnection);
+                    }else{
+                        console.error("Create user error!")
+                    }    
+                }
+                catch (err) {
+                    console.error("Sign up failed!" + err.message)
+                    res.sendStatus(500);
+                }
+                //TODO Retourner les informations de connexion de l'utilisateur (voir interface UserConnection) sous format json 
+                
+            }
 
-            //TODO Ajouter l'utilisateur à la BD 将用户添加到数据库中
-            //Retounez un code 500 en cas de problème 在出现问题的情况下返回代码500
-
-            //TODO Générer le jeton de l'utilisateur à l'aide du service auth.service 使用auth.service生成用户的令牌
-            
-            //TODO Retourner les informations de connexion de l'utilisateur (voir interface UserConnection) sous format json 
-            // 以json格式返回用户的连接信息（见UserConnection接口）。
-            res.json();
         });
-        
+
         return router;
     }
 
--- server/src/services/mongodb.service.ts
+++ server/src/services/mongodb.service.ts
@@ -1,5 +1,5 @@
 import { injectable } from 'inversify';
-import { Collection, MongoClient, WithId } from 'mongodb';
+import { Collection, MongoClient, W, WithId } from 'mongodb';
 import { User } from '../interfaces';
 
 const url = 'mongodb://127.0.0.1:27017';
@@ -12,37 +12,35 @@ export class MongodbService {
 
     private _client: MongoClient = new MongoClient(url);
     private _collection: Collection<User>;
-    
-    constructor(){
+
+    constructor() {
         this._client.connect();
         //Collection à utiliser
         this._collection = this._client.db('tp2').collection<User>('users');
     }
-    
-    //Retourne les informations d'un utilisateur à partir de son username
-    async getUserByUsername(username: string):Promise<WithId<User> | null>{
-        console.error("getUserByUsername")
-        
-        //TODO Trouver l'utilisateur en fonction de son nom d'utilisateur
-        // 通过用户名查找用户
-        const user = await this._collection.find({}).toArray();
-        console.log(user[0])
-        
-        //TODO Retourner l'utilisateur avec son _id
-        // 返回用户与它的_id
-        throw new Error('Not implemented method');
-    }
-    
-    //Fait la création d'un utilisateur dans la base de données
-    async createUser(user: User): Promise<WithId<User> | null>{
-        console.error("createUser")
-        throw new Error('Not implemented method');
-        //TODO Créer un utilisateur en fonction des information d'authentification
-        //Utilisez l'interface User根据认证信息创建一个用户
-        //使用用户接口
 
-        //TODO Retourner le user créé avec son _id
-        // 返回创建的用户和它的_id
+    //Retourne les informations d'un utilisateur à partir de son username
+    async getUserByUsername(username: string): Promise<WithId<User> | null> {
+
+        const user: Promise<WithId<User> | null> = await this._collection.find({ 'username': { username } }).limit(1)[0];
+        console.log('%cmongodb.service.ts line:26 user', 'color: #007acc;', user);
+        return user;
+    }
+
+    //Fait la création d'un utilisateur dans la base de données
+    async createUser(user: User): Promise<WithId<User> | null> {
+        console.error("Test: createUser")
+        const userToAdd: User = {
+            hash: user.hash,
+            username: user.username
+        }
+        // try {
+            this._collection.insertOne(userToAdd)
+            console.log("create succeed");
+            //Retourner le user créé avec son _id
+            const userNew : Promise<WithId<User>>= await this._collection.find({ username: user.username }).limit(1)[0];
+            console.log('%cmongodb.service.ts line:49 userNew', userNew);
+            return userNew;
     }
 
 }
